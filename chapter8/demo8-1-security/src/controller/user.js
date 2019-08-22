36672c25b73be4df34c5b4117ac1116a4d31805a
jinjian <jinj116@chinaunicom.cn>
2019-08-22 19:25:13 +0800

    8-22 commit

--- chapter8/demo8-1-security/src/controller/user.js
+++ chapter8/demo8-1-security/src/controller/user.js
@@ -1,11 +1,8 @@
     
-    const { exec, escape} = require('../db/mysql');
+const { exec, escape} = require('../db/mysql');
 const login = (username = '', password = '') => {
-    // 
-    console.log(username, password);
     username = escape(username);
     password = escape(password);
-    console.log('escape',username, password);
     const sql = `select username, realname from users where username = ${username} and password = ${password}`;
     return exec(sql).then(rows => {
         return rows[0] || {};
