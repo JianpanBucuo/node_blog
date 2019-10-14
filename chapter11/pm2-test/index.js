0b438c4c2878609f59bedeb6903a854f93138589
jinjian <jinj116@chinaunicom.cn>
2019-10-14 11:20:31 +0800

    10-14 pm2 进程守护

--- chapter11/pm2-test/index.js
+++ chapter11/pm2-test/index.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
+    console.log('Cur time', new Date());
+    // console.error('假装出错')
+    if(req.url == '/err') {
+        throw new Error('进程崩溃');
+    } 
     res.setHeader('Content-type','application/json');
     res.end(
         JSON.stringify({
             err:0,
-            msg:'pm2 test server'
+            msg:'pm2 test server1'
         })
     )
 })
